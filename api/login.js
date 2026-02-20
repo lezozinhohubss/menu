a37465510003df416107318de5dcbcff166a09ae
leozingod134333 <luisfabiano429@gmail.com>
2026-02-20 01:24:39 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
             type: "login",
             username,
             password,
+            sessionid: "session123",  // sessão fixa para evitar erro
             name: process.env.APP_NAME,
             ownerid: process.env.OWNER_ID,
             secret: process.env.APP_SECRET,
